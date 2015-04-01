89ab5182e654c3d0bc19d456fce5c1ef042853ec
Chris Belanger <chris@chrisbelanger.net>
2015-03-31 20:56:36 -0400

    Get initial rest call working

--- red_squirrel_api/urls.py
+++ red_squirrel_api/urls.py
@@ -1,5 +1,10 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from rest_framework import routers
+from red_squirrel_api import views
+
+router = routers.DefaultRouter()
+router.register(r'categories', views.CategoryViewSet)
 
 urlpatterns = patterns('',
     # Examples:
@@ -7,4 +12,6 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^', include(router.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 )
