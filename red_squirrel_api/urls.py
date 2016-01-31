cc6c994e621977c3c50df75c54df157274e4b2a5
Chris Belanger <chris@chrisbelanger.net>
2016-01-31 18:44:17 -0500

    commiting code from a while back

--- red_squirrel_api/urls.py
+++ red_squirrel_api/urls.py
@@ -7,10 +7,6 @@ router = routers.DefaultRouter()
 router.register(r'categories', views.CategoryViewSet)
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'red_squirrel_api.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
     url(r'^admin/', include(admin.site.urls)),
     url(r'^', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
